32ce515d773e03a8451436951522de226698a345
Maxence B. <maxence.barre59@gmail.com>
2024-02-29 15:39:15 +0100

    ouverture de l'explorateur de fichier pour la selection d'une video

--- configure_extract_phase.py
+++ configure_extract_phase.py
@@ -34,14 +34,20 @@ class MainWindow(qt.QMainWindow):
         super().__init__()
 
         self.setupUI()
-
+        self.take_photo_cam = True
         # Initialize webcam
         print("[INFO] Recherche des caméras disponibles")
         ad_serial = Thorlabs.list_cameras_tlcam()  # affiche normalement la liste des caméras connectées
-        print(f"[INFO] Adresses trouvées: {ad_serial}")
-        self.cam = Thorlabs.ThorlabsTLCamera(serial=ad_serial[0])
-        self.cam.set_exposure(10E-3)
-        print(f"[INFO] Caméra {ad_serial[0]} connectée")
+        if len(ad_serial) != 0:
+            self.cam_zelux = True
+            print(f"[INFO] Adresses trouvées: {ad_serial}")
+            self.cam = Thorlabs.ThorlabsTLCamera(serial=ad_serial[0])
+            self.cam.set_exposure(10E-3)
+            print(f"[INFO] Caméra {ad_serial[0]} connectée")
+        else:
+            self.cam_zelux = False
+            print("[WARNING] Caméra Zelux non trouvée, Ouverture de la caméra par défaut")
+            self.cam = cv2.VideoCapture(0)
 
         if use_webcam:
             self.cap = cv2.VideoCapture(0)
@@ -58,6 +64,19 @@ class MainWindow(qt.QMainWindow):
         # Create a central widget
         self.centralWidget = qt.QWidget()
         self.setCentralWidget(self.centralWidget)
+
+        # create menuBar for selection
+        menuBar = self.menuBar()
+        choix_source = menuBar.addMenu('Source')
+        video = qt.QAction("Vidéo", self)
+        choix_source.addAction(video)
+        video.triggered.connect(self.config_video)
+        choix_source.addAction("Caméra")
+        action = menuBar.addMenu('Action')
+        action.addAction('Lancer Traitement')
+        action.addAction('Sauvegarder Data')
+        
+
 
         # Create a layout for the central widget
         grid = qt.QGridLayout()
@@ -91,6 +110,20 @@ class MainWindow(qt.QMainWindow):
         self.plotView3 = pg.PlotWidget()
         grid.addWidget(self.plotView3, 1, 1)
 
+    def config_video(self):
+        print("[INFO] Sélection d'une vidéo ou d'un fichier h5 comme outil de travail")
+        self.take_photo_cam = False
+        dlg = qt.QFileDialog()
+        dlg.setFileMode(qt.QFileDialog.AnyFile)
+
+            
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+            if len(filenames) > 1:
+                print("[WARNING] Un seul fichier maximum")
+                filenames = self.config_video()
+        self.take_photo_cam = True
+        return filenames
 
     def update_graph_spectre_fft(self, image_sommee):
         # on fait d'abord la fft
@@ -98,21 +131,24 @@ class MainWindow(qt.QMainWindow):
         sp = np.fft.fft(image_sommee)[freq >= 0]
         freq = freq[freq >= 0]
         
-        print("sp", sp.shape, "freq", freq.shape)
+        # print("sp", sp.shape, "freq", freq.shape)
         self.plotView3.clear()
         self.plotView3.plot(freq, np.abs(sp))
 
 
     def update_graph_sum(self, image_roi):
-        print(image_roi.shape)
-        sommation = np.sum(image_roi, axis=1)
-        print(sommation.shape)
+        # print(image_roi.shape)
+        if len(image_roi.shape) == 3: # image en couleur
+            sommation = np.sum(image_roi[:, :, 0], axis=1)
+        else:
+            sommation = np.sum(image_roi, axis=1)
+        # print(sommation.shape)
         self.plotView2.clear()
         self.plotView2.plot(sommation)
         self.update_graph_spectre_fft(sommation)
     
     def update_roi(self):
-        print("updating roi")
+        # print("updating roi")
         image_roi = np.rot90(self.roi.getArrayRegion(self.current_frame, self.graphicsView))
         self.graphicsView2.setImage(image_roi)
         self.graphicsView2.autoRange()
@@ -120,11 +156,17 @@ class MainWindow(qt.QMainWindow):
 
 
     def update_frame(self):
-        print("begin update")
-        self.current_frame = self.cam.snap()
-        self.graphicsView.setImage(np.rot90(self.current_frame))
-        self.update_roi()
-        print("end update")
+        # print("begin update")
+        if self.take_photo_cam:
+            if self.cam_zelux:
+                self.current_frame = np.rot90(self.cam.snap())
+            else:
+                ret, self.current_frame = self.cam.read()
+                # print('r', ret)
+                self.current_frame = np.rot90(np.rot90(np.rot90(cv2.cvtColor(self.current_frame, cv2.COLOR_BGR2RGB))))
+            self.graphicsView.setImage(self.current_frame)
+            self.update_roi()
+        # print("end update")
 
         if use_webcam:
             ret, frame = self.cap.read()
